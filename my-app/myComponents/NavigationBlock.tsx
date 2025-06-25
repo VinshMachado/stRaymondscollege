a8f48da7821c1c97b6122d663574445390674593
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2025-06-25 18:33:12 +0530

    nav bar details

--- my-app/myComponents/NavigationBlock.tsx
+++ my-app/myComponents/NavigationBlock.tsx
@@ -16,98 +16,74 @@ export function NavigationBlock() {
   return (
     <NavigationMenu viewport={false}>
       <NavigationMenuList>
-        <NavigationMenuItem>
+      <NavigationMenuItem>
           <NavigationMenuTrigger className="bg-red-900 text-white text-xl hover:bg-red-900">
-            Simple
+            About Us
           </NavigationMenuTrigger>
           <NavigationMenuContent className="bg-white">
             <ul className="grid w-[200px] gap-4 bg-white">
               <li className="bg-white">
                 <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#">Components</Link>
+                  <Link href="#">Vision and Mission</Link>
                 </NavigationMenuLink>
                 <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#">Documentation</Link>
+                  <Link href="#">History</Link>
                 </NavigationMenuLink>
                 <NavigationMenuLink asChild className="bg-white">
                   <Link href="#" className="bg-white">
-                    Blocks
+                    Silver jubilee
                   </Link>
                 </NavigationMenuLink>
+                <NavigationMenuLink asChild className="bg-white">
+                  <Link href="#">Message from the Principal</Link>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+
+
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="bg-red-900 text-white text-xl hover:bg-red-900">
+            Curricular
+          </NavigationMenuTrigger>
+          <NavigationMenuContent className="bg-white">
+            <ul className="grid w-[200px] gap-4 bg-white">
+              <li className="bg-white">
+                <NavigationMenuLink asChild className="bg-white">
+                  <Link href="#">Environment Day</Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild className="bg-white">
+                  <Link href="#">Annual Day</Link>
+                </NavigationMenuLink>
+                
               </li>
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
         <Link href="/home" className="text-white text-xl m-10">
+         Streams Offered
+        </Link>
+
+        <Link href="/home" className="text-white text-xl m-10">
           Contact Us
         </Link>
 
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="bg-red-900 text-white text-xl hover:bg-red-900">
-            Simple
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className="bg-white">
-            <ul className="grid w-[200px] gap-4 bg-white">
-              <li className="bg-white">
-                <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#">Components</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#">Documentation</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#" className="bg-white">
-                    Blocks
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="bg-red-900 text-white text-xl hover:bg-red-900">
-            Simple
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className="bg-white">
-            <ul className="grid w-[200px] gap-4 bg-white">
-              <li className="bg-white">
-                <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#">Components</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#">Documentation</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#" className="bg-white">
-                    Blocks
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="bg-red-900 text-white text-xl hover:bg-red-900">
-            Simple
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className="bg-white">
-            <ul className="grid w-[200px] gap-4 bg-white">
-              <li className="bg-white">
-                <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#">Components</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#">Documentation</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild className="bg-white">
-                  <Link href="#" className="bg-white">
-                    Blocks
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
+        <Link href="/home" className="text-white text-xl m-10">
+          Join Us
+        </Link>
+
+        <Link href="/home" className="text-white text-xl m-10">
+          Gallery
+        </Link>
+
+        <Link href="/home" className="text-white text-xl m-10">
+          Updates
+        </Link>
+
+        
       </NavigationMenuList>
     </NavigationMenu>
   );
