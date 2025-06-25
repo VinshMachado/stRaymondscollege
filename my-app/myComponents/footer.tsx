3d301838a66876e6de4017d0f5b21814a38efb88
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2025-06-25 20:38:22 +0530

    responsive?

--- my-app/myComponents/footer.tsx
+++ my-app/myComponents/footer.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Image from 'next/image'
 const footer = () => {
   return (
-    <div className="bg-red-900 text-white flex justify-center items-center py-10">
+    <div className="bg-red-900 h-auto text-white flex justify-center items-center py-10">
         
-    <Image src="/Logo.png" alt="logo" width={120} height={50} />
-    <div className='border-l border-white h-36 mx-10 px-5 pt-4'>
+    <Image src="/Logo.png" alt="logo" width={120} height={50} className='hidden sm:block'/>
+    <div className='border-l sm:border-white border-red-900 h-36 mx-10 px-5 pt-4 ' >
     <p className='text-2xl font-bold'>Contact Us</p>
     <p className='text-md'>St. Raymond's PU College,</p>
   <p className='text-md'>Bangalore, Karnataka, India</p>
