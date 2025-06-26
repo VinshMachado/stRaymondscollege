3ae3099339b499011216f3a834f188dd9009fb94
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-06-26 14:16:35 +0530

    mission and vision ,jblee

--- my-app/app/silver/page.tsx
+++ my-app/app/silver/page.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
-
+import { Jublee } from "@/myComponents/Jublee";
+import React from "react";
+import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="w-full h-auto flex flex-col justify-center items-center">
+      <Jublee />
 
-export default page
+      <Card className=" shadow-none mt-10 border-0">
+        <CardContent className="p-6">
+          <div className="flex flex-col items-center mb-6">
+            <h2 className="text-3xl font-bold  text-red-800 text-center mt-4">
+              ಬೆಳ್ಳಿ ಹಬ್ಬದ ಲಾಂಛನದ ವಿವರಣೆ
+            </h2>
+          </div>
+
+          <p className="text-xl text-gray-800 leading-relaxed p-5">
+            ಸೇಂಟ್ ರೈಮಂಡ್ ಪದವಿ ಪೂರ್ವ ಕಾಲೇಜು , ವಾಮಂಜೂರು ಇದರ ಬೆಳ್ಳಿ ಹಬ್ಬದ ಲಾಂಛನವು
+            ನಮ್ಮ ಶಿಕ್ಷಣ ದರ್ಶನದ ಆಳವಾದ ಅರ್ಥವನ್ನು ಪ್ರತಿಬಿಂಬಿಸುತ್ತದೆ. ಈ ಲಾಂಛನದಲ್ಲಿ
+            ಮಧ್ಯಭಾಗದಲ್ಲಿರುವ ಮರವು ವಿದ್ಯೆಯ ಪ್ರತೀಕವಾಗಿದ್ದು, ಮೌಲ್ಯಾಧಾರಿತ ಶಿಕ್ಷಣದ
+            ಮೂಲಕ ವಿದ್ಯಾರ್ಥಿಗಳ ಸಮಗ್ರ ವ್ಯಕ್ತಿತ್ವವನ್ನು ಬೆಳೆಸುವ ನಮ್ಮ ದೃಷ್ಟಿಕೋನವನ್ನು
+            ಪ್ರತಿನಿಧಿಸುತ್ತದೆ.
+            <br />
+            <br />
+            ಮರದ ಎಲೆಗಳು ಬೆಳವಣಿಗೆಯ ಚಿಹ್ನೆಯಾಗಿದ್ದು, ವೃತ್ತಿಪರ, ಬೌದ್ಧಿಕ ಹಾಗೂ ಮಾನವೀಯ
+            ಮೌಲ್ಯಗಳಲ್ಲಿ ವಿದ್ಯಾರ್ಥಿಗಳು ಬೆಳೆಯುವುದನ್ನು ಸೂಚಿಸುತ್ತವೆ. ಗಿಡದ ಮೇಲೆ ಕಾಣುವ
+            ಪಠ್ಯಪೆಟ್ಟಿಗೆ, ಪದವಿಪೂರ್ವ ಟೋಪಿ, ಚಕ್ರ, ಮುಂತಾದ ಚಿಹ್ನೆಗಳು ವಿವಿಧ
+            ಶಾಖೆಗಳಲ್ಲಿ ನೀಡಲಾಗುವ ಸಂಪೂರ್ಣ ಶಿಕ್ಷಣವನ್ನು ಸೂಚಿಸುತ್ತವೆ.
+            <br />
+            <br />
+            ಮರದ ಕೆಳಭಾಗದಲ್ಲಿ ಇರುವ ವಿದ್ಯಾರ್ಥಿಗಳ ಚಿತ್ರಣದಿಂದ ನಾವು ಎಲ್ಲ
+            ವಿದ್ಯಾರ್ಥಿಗಳಲ್ಲಿಯೂ ಸಾಮರ್ಥ್ಯವನ್ನು ಗುರುತಿಸಿ, ಅವರನ್ನು ಉಜ್ವಲ ಭವಿಷ್ಯದತ್ತ
+            ನಯವಾಗಿ ಕರೆದೊಯ್ಯುವ ನಿಶ್ಚಯವನ್ನು ತೋರಿಸುತ್ತದೆ. ಲಾಂಛನದಲ್ಲಿರುವ 'Rooted in
+            Values, Education towards Excellence' ಎಂಬ ಧ್ಯೇಯವಾಕ್ಯವು ಮೌಲ್ಯಗಳಲ್ಲಿ
+            ಆಧಾರಿತ ಶಿಕ್ಷಣದ ಮೂಲಕ ಶ್ರೇಷ್ಠತೆಗೆ ನಾವೆಲ್ಲಾ ಕೈಜೋಡಿಸುತ್ತೇವೆ ಎಂಬ ಬದ್ಧತೆಯ
+            ತೋರುವಿಕೆ.
+            <br />
+            <br />
+            "ನಮ್ಮ ಕಾಲೇಜಿನ ಲೋಗೋವನ್ನು ಪರಿಚಯಿಸಲು ನನಗೆ ಸಂತೋಷವಾಗುತ್ತಿದೆ. ಲೋಗೋವು ನಾನಾ
+            ದಿಕ್ಕುಗಳಲ್ಲಿ ಬೆಳೆಯುವ ವಿದ್ಯಾರ್ಥಿಗಳ ಯಶಸ್ವಿ ಯಾನವನ್ನು, ಹಾಗೂ ನಮ್ಮ ಕಾಲೇಜು
+            ನೀಡುತ್ತಿರುವ ಉನ್ನತ ಮಟ್ಟದ ಮೌಲ್ಯಾಧಾರಿತ ಶಿಕ್ಷಣವನ್ನು ಸಮರ್ಥವಾಗಿ
+            ಪ್ರತಿನಿಧಿಸುತ್ತದೆ.
+            <br />
+            <br />
+            '25 Glorious Years of Transformative Education for fullness of life
+            for all' ಎಂಬ ಸಾಲು ಸೇವೆಯ 25ನೇ ವರ್ಷಕ್ಕೆ ಕಾಲಿಟ್ಟಿರುವುದು ಹೆಮ್ಮೆಯ
+            ಪ್ರತೀಕವಾಗಿದೆ.
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default page;
