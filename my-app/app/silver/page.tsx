ce73a5a4bb13478be5bef49baeaad96d7f2c9ddd
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-06-26 14:23:11 +0530

    err

--- my-app/app/silver/page.tsx
+++ my-app/app/silver/page.tsx
@@ -1,6 +1,6 @@
 import { Jublee } from "@/myComponents/Jublee";
 import React from "react";
-import Image from "next/image";
+
 import { Card, CardContent } from "@/components/ui/card";
 const page = () => {
   return (
