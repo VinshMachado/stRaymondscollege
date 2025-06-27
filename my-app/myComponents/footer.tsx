2687b967032607df6a04727a0762309dbf99f481
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2025-06-27 18:39:42 +0530

    final few things done

--- my-app/myComponents/footer.tsx
+++ my-app/myComponents/footer.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import Image from "next/image";
 const footer = () => {
   return (
-    <div className="bg-red-900 text-white flex justify-center items-center py-10">
+    <div className="bg-red-900 h-[20rem] text-white flex justify-center items-center py-10">
       <Image src="/Logo.png" alt="logo" width={120} height={50} className="hidden sm:block"/>
-      <div className="border-l sm:border-white border-red-900 h-36 mx-10 px-5 pt-4">
+      <div className="border-l sm:border-white border-red-900 h- mx-10 px-5 pt-4">
         <p className="text-2xl font-bold mb-2">Contact Us</p>
         <p className="text-md">St. Raymond's PU College,</p>
-        <p className="text-md">Bangalore, Karnataka, India</p>
-        <p className="text-md">Phone: +91 123 456 7890</p>
-        <p className="text-md">Email: info@straymonds.edu.in</p>
+        <p className="text-md">Vamanjoor,Kudupu,Thiruvail,Karnataka 575028</p>
+        <p className="text-md">Phone: +91 988 060 3858</p>
+        <p className="text-md">Phone: +91 636 050 8493</p>
+        <p className="text-md">Email: straymondspucollege@gmail.com</p>
       </div>
     </div>
   );
