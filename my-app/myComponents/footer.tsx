ad1c6ac4e825aebf1096339363170d25cb76d4ac
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2025-06-28 13:53:59 +0530

    footer added number and promo creds

--- my-app/myComponents/footer.tsx
+++ my-app/myComponents/footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 const footer = () => {
   return (
+    <div>
     <div className="bg-red-900 h-[20rem] text-white flex justify-center items-center py-10">
       <Image src="/Logo.png" alt="logo" width={120} height={50} className="hidden sm:block"/>
       <div className="border-l sm:border-white border-red-900 h- mx-10 px-5 pt-4">
@@ -10,10 +11,15 @@ const footer = () => {
         <p className="text-md">Vamanjoor,Kudupu,Thiruvail,Karnataka 575028</p>
         <p className="text-md">Phone: +91 988 060 3858</p>
         <p className="text-md">Phone: +91 636 050 8493</p>
-        <p className="text-md">Email: straymondspucollege@gmail.com</p>
+        <p className="text-md">Email: srpucv2001@gmail.com</p>
       </div>
+    </div>
+    <div className="bg-white text-black text-center font-semibold ">
+      <p>Website developed by Sion and Vinish contact +91 95130 97816 for Website development.</p>
+    </div>
     </div>
   );
 };
 
 export default footer;
+
